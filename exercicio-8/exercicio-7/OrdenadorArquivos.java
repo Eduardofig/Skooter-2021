49dd2d79b2427a0f77f3444203ea4c54ca74805a
Eduardofig <duzinho039@gmail.com>
2021-06-08 21:45:08 +0000

    exercicio-7 pronto

--- exercicio-8/exercicio-7/OrdenadorArquivos.java
+++ exercicio-8/exercicio-7/OrdenadorArquivos.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class OrdenadorArquivos
@@ -20,28 +19,33 @@ public class OrdenadorArquivos
     {
         File diretorioAlvo = null;
         File[] arquivos;
-
-        final ArrayList<String> al= new ArrayList<String>();
+        String ordem, nomeArquivo;
 
         try
         {      
-            // Abre o Diretorio
+            //Abre o Diretorio
             diretorioAlvo = new File(this.getDiretorio());
             //Cria o Array de arquivos contidos no diretorio e os faz o filtro
             //para manter somente os que possuem a extensao .mpe
             arquivos = Arrays.stream(diretorioAlvo.listFiles()).filter(arquivo -> arquivo.getName().endsWith(".mp3")).toArray(File[]::new);
-            ArrayList<File> listaDeArquivosOrdenados = new ArrayList<File>();
             Pair[] Pares = new Pair[arquivos.length];
             for (int i = 0; i < arquivos.length; i++) Pares[i] = new Pair(arquivos[i]);
 
             Arrays.sort(Pares);
 
-            // Faz um array com os apenas os arquivos ordenados do array de pares
-            for (int i = 0; i < arquivos.length; i++){
-                arquivos[i] = Pares[i].arquivo;
+            //Faz um array com os apenas os arquivos ordenados do array de pares
+            for (int i = 0; i < arquivos.length; i++) {
+                //Rename dos arquivos
+                nomeArquivo = Pares[i].arquivo.getName().replace("-", "");
+                for (int j = 0; j < 10; j++) nomeArquivo = nomeArquivo.replace(String.valueOf(j), "");
+                ordem = String.valueOf(i + 1);
+                //Criacao do contador de 4 algarismos
+                if(i < 999) ordem = '0' + ordem; if(i < 99) ordem = '0' + ordem; if(i < 9) ordem = '0' + ordem;
+                nomeArquivo = this.getDiretorio() + ordem + nomeArquivo + '3';
+                Pares[i].arquivo.renameTo(new File(nomeArquivo));
+                arquivos[i] = new File(nomeArquivo);
                 // Print dos arquivos ordenados com os seus respectivos tamanhos
                 System.out.println(arquivos[i].getName() + " - " + arquivos[i].length());
-                al.add(arquivos[i].getName());
             }
         }
         catch(Exception e)
@@ -50,8 +54,10 @@ public class OrdenadorArquivos
             e.printStackTrace();
         }
     }
+
     OrdenadorArquivos(String diretorio) 
     {
+        if(!diretorio.endsWith("/")) diretorio += '/';
         setDiretorio(diretorio);
     }
 }
