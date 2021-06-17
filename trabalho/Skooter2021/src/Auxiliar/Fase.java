60abbc2abf0856ee5116639f2428380629461648
Eduardofig <duzinho039@gmail.com>
2021-06-17 22:13:57 +0000

    Funcionalidade de backup e reset na classe Fase

--- trabalho/Skooter2021/src/Auxiliar/Fase.java
+++ trabalho/Skooter2021/src/Auxiliar/Fase.java
@@ -4,7 +4,11 @@ import Modelo.Elemento;
 import Modelo.Hero;
 
 public class Fase implements Serializable {
-    //Implementar backups
+    //Backups para o reset da fase apos o heroi morrer
+    protected ArrayList<Elemento> listaBlocosInteragiveisBackup;
+    protected ArrayList<Elemento> listaInimigosBackup;
+    protected ArrayList<Elemento> listaBlocosNaoInteragiveisBackup;
+    //Elementos que serao utilizados
     protected ArrayList<Elemento> listaBlocosInteragiveis;
     protected ArrayList<Elemento> listaInimigos;
     protected ArrayList<Elemento> listaBlocosNaoInteragiveis;
@@ -38,22 +42,34 @@ public class Fase implements Serializable {
         return this.listaBlocosNaoInteragiveis;
     }
 
+    public void backup() {
+        this.listaBlocosNaoInteragiveisBackup = new ArrayList<Elemento>(this.listaBlocosNaoInteragiveis);
+        this.listaBlocosInteragiveisBackup = new ArrayList<Elemento>(this.listaBlocosInteragiveis);
+        this.listaInimigosBackup = new ArrayList<Elemento>(this.listaInimigos);
+    }
+
+    public void reset() {
+        this.listaBlocosNaoInteragiveis = new ArrayList<Elemento>(this.listaBlocosNaoInteragiveisBackup);
+        this.listaBlocosInteragiveis = new ArrayList<Elemento>(this.listaBlocosInteragiveisBackup );
+        this.listaInimigos = new ArrayList<Elemento>(this.listaInimigosBackup);
+    }
+
     public ArrayList<Elemento> getElementos() {
         ArrayList<Elemento> eElementos = new ArrayList<Elemento>();
         eElementos.add(this.hHero);
-        for(int i = 0; i < this.listaBlocosNaoInteragiveis.size(); i++) {
-            eElementos.add(this.listaBlocosNaoInteragiveis.get(i));
+        for(int i = 0; i < this.listaBlocosNaoInteragiveisBackup.size(); i++) {
+            eElementos.add(this.listaBlocosNaoInteragiveisBackup.get(i));
         }
-        for(int i = 0; i < this.listaBlocosInteragiveis.size(); i++) {
-            eElementos.add(this.listaBlocosInteragiveis.get(i));
+        for(int i = 0; i < this.listaBlocosInteragiveisBackup.size(); i++) {
+            eElementos.add(this.listaBlocosInteragiveisBackup.get(i));
         }
-        for(int i = 0; i < this.listaInimigos.size(); i++) {
-            eElementos.add(this.listaInimigos.get(i));
+        for(int i = 0; i < this.listaInimigosBackup.size(); i++) {
+            eElementos.add(this.listaInimigosBackup.get(i));
         }
         return eElementos;
     }
 
-    public Fase (Hero hHero) {
+    public Fase(Hero hHero) {
         this.setHero(hHero);
     }
     
