2a975f717e20ed4d72ac8e1544b6ad3aefb685c6
clark25 <jonatas_s.v.s@hotmail.com>
2021-06-17 15:57:14 -0300

    Criacao das minhas classes (jonatas)

---
+++ trabalho/Skooter2021/src/Modelo/BlocoNaoInteragivel.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import Auxiliar.Consts;
+import Auxiliar.Desenhador;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author jonatas
+ */
+public class BlocoNaoInteragivel extends Elemento implements Serializable {
+    public BlocoNaoInteragivel(String sNomeImagePNG) {
+        super(sNomeImagePNG);
+        this.setbTransponivel(false);
+        this.iContaFrames = 0;
+    }
+}
---
+++ trabalho/Skooter2021/src/Modelo/RoboInimigo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import Auxiliar.Consts;
+import Auxiliar.Desenhador;
+import Auxiliar.Posicao;
+import Modelo.BlocoInteragivel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jonatas
+ */
+public class RoboInimigo extends Elemento implements Serializable{
+    
+}
