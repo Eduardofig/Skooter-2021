b9ab487f239aa189cf68e2ec3bde87ec92713c48
Eduardofig <duzinho039@gmail.com>
2021-06-21 22:39:51 +0000

    sprites heroi energizado

--- trabalho/Skooter2021/src/Data/Fases.java
+++ trabalho/Skooter2021/src/Data/Fases.java
@@ -46,10 +46,10 @@ public class Fases {
         aHeroiSprites.add("heroi" + File.separator + "heroi_left.png");
         aHeroiSprites.add("heroi" + File.separator + "heroi_up.png");
         aHeroiSprites.add("heroi" + File.separator + "heroi_right.png");
-        aHeroiSprites.add("inimigos" + File.separator + "inimigo_vermelho_down.png");
-        aHeroiSprites.add("inimigos" + File.separator + "inimigo_vermelho_left.png");
-        aHeroiSprites.add("inimigos" + File.separator + "inimigo_vermelho_up.png");
-        aHeroiSprites.add("inimigos" + File.separator + "inimigo_vermelho_right.png");
+        aHeroiSprites.add("heroi" + File.separator + "robo_energizado_down.png");
+        aHeroiSprites.add("heroi" + File.separator + "robo_energizado_left.png");
+        aHeroiSprites.add("heroi" + File.separator + "robo_energizado_up.png");
+        aHeroiSprites.add("heroi" + File.separator + "robo_energizado_right.png");
 
         aRoboAmareloSprites.add("inimigos" + File.separator + "inimigo_amarelo_down.png");
         aRoboAmareloSprites.add("inimigos" + File.separator + "inimigo_amarelo_left.png");
