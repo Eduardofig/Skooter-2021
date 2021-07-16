e067b18337ada2724e8d60fb78b9269d565ae669
Eduardofig <duzinho039@gmail.com>
2021-07-16 22:15:07 +0000

    :art: STUCTURE: Mais legibilidade no output

--- exercicio-11/exercicio-3/Main.java
+++ exercicio-11/exercicio-3/Main.java
@@ -11,15 +11,17 @@ public class Main {
         t.inserir(Integer.valueOf(20));
         t.inserir(Integer.valueOf(12));
         t.inserir(Integer.valueOf(39));
-        
 
+        System.out.println("\nPrint da arvore antes das remocoes:");
         t.printOrdenado();
 
+        System.out.println("\nBuscas antes das remocoes:");
         System.out.println(t.find(Integer.valueOf(15)));
         System.out.println(t.find(Integer.valueOf(7)));
         System.out.println(t.find(Integer.valueOf(9)));
         System.out.println(t.find(Integer.valueOf(12)));
         System.out.println(t.find(Integer.valueOf(1)));
+
 
         t.delete(Integer.valueOf(7));
         t.delete(Integer.valueOf(15));
@@ -28,8 +30,10 @@ public class Main {
         t.delete(Integer.valueOf(20));
         t.delete(Integer.valueOf(39));
 
+        System.out.println("\nPrint da arvore depois das remocoes:");
         t.printOrdenado();
 
+        System.out.println("\nBuscas depois das remocoes:");
         System.out.println(t.find(Integer.valueOf(15)));
         System.out.println(t.find(Integer.valueOf(7)));
         System.out.println(t.find(Integer.valueOf(9)));
