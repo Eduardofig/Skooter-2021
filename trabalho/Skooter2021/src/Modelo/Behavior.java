894b9a5c880d8334f19686dc77d9d2955cb47639
Eduardofig <duzinho039@gmail.com>
2021-07-22 19:04:35 +0000

    :sparkles: NEW FEATURE: Classes Behavior e ElementoGenerico
    Novas classes para a implementacao do design pattern bridge

---
+++ trabalho/Skooter2021/src/Modelo/Behavior.java
@@ -0,0 +1,13 @@
+package Modelo;
+
+import java.io.Serializable;
+import Auxiliar.Posicao;
+
+/**
+ *
+ * @author Eduardo, Jonatas
+ */
+
+public abstract class Behavior implements Serializable{
+    public abstract void agir(Posicao pPosicao);
+}
---
+++ trabalho/Skooter2021/src/Modelo/ElementoGenerico.java
@@ -0,0 +1,20 @@
+package Modelo;
+
+/**
+ *
+ * @author Eduardo, Jonatas
+ */
+
+public class ElementoGenerico extends Elemento {
+
+    protected Behavior bBehavior;
+
+    public void acao() {
+        this.bBehavior.agir(this.getPosicao());
+    }
+
+    public ElementoGenerico(String sNomeImagePNG, int linha, int coluna, Behavior sBehavior) {
+        super(sNomeImagePNG, linha, coluna);
+        this.bBehavior = sBehavior;
+    }
+}
