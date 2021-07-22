dbf1d8371530c91d94aa792d30639615b7375d9f
Eduardofig <duzinho039@gmail.com>
2021-07-22 19:23:39 +0000

    :sparkles: NEW FEATURE: Behavior teletransportador

---
+++ trabalho/Skooter2021/src/Modelo/BehaviorTeletransportador.java
@@ -0,0 +1,38 @@
+package Modelo;
+
+import Auxiliar.Posicao;
+import java.util.Random;
+
+/**
+ *
+ * @author Eduardo, Jonatas
+ */
+
+public class BehaviorTeletransportador extends Behavior {
+
+    protected Random rRandom;
+
+    public void agir(Posicao pPosicao) {
+        int move = this.rRandom.nextInt();
+        for(int i = 0; i < 3; ++i) {
+            switch(move) {
+                case 0:
+                    pPosicao.moveLeft();
+                    break;
+                case 1:
+                    pPosicao.moveRight();
+                    break;
+                case 2:
+                    pPosicao.moveUp();
+                    break;
+                default:
+                    pPosicao.moveDown();
+                    break;
+            }
+        }
+    }
+     
+    public BehaviorTeletransportador() {
+        this.rRandom = new Random();
+    }
+}
