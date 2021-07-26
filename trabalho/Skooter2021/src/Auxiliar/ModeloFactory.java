5325ad7a07107c8cb98b885847c6ef58fd4df8cf
Eduardofig <duzinho039@gmail.com>
2021-07-26 20:23:36 +0000

    :recycle: REFACTOR: Implementacao do factory no fases

--- trabalho/Skooter2021/src/Auxiliar/ModeloFactory.java
+++ trabalho/Skooter2021/src/Auxiliar/ModeloFactory.java
@@ -10,13 +10,17 @@ public class ModeloFactory {
         switch(seletor) {
             case "Colecionavel":
                 return new Colecionavel(sNomeImagePNG, linha, coluna);
+            case "BlocoNaoInteragivel":
+                return new BlocoNaoInteragivel(sNomeImagePNG, linha, coluna);
+            case "BlocoInteragivel":
+                return new BlocoInteragivel(sNomeImagePNG, linha, coluna);
             default:
                 return new Coracao(sNomeImagePNG, linha, coluna);
         }
     }
 
     //Method overloading para Elementos do tipo BlocoNaoInteragivel
-    public Elemento criarElemento(String seletor, String sNomeImagePNG, int linha, int coluna, int sOrientacao) {
+    public Elemento criarElemento(String seletor, String sNomeImagePNG, int sOrientacao, int linha, int coluna) {
         switch (seletor) {
             default:
                 return new BlocoSeta(sNomeImagePNG, sOrientacao, linha, coluna);
